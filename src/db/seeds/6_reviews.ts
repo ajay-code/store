b4de04bef4695f62cd54115e3b68594e0f5472f3
Ajay Singh <ajay10mar96@gmail.com>
2023-01-26 13:08:32 +0530

    adding review seeder

---
+++ src/db/seeds/6_reviews.ts
@@ -0,0 +1,21 @@
+import { DBTableList } from '#src/lib/knex/db.js'
+import { Knex } from 'knex'
+import reviews from '../data/reviews.json' assert { type: 'json' }
+
+export const seed = async (knex: Knex) => {
+    const reviewList = []
+
+    let id = 1
+    for (let review of reviews) {
+        reviewList.push({
+            id,
+            author: review.author,
+            store: review.store,
+            text: review.text,
+            rating: review.rating,
+        })
+        id++
+    }
+
+    await knex.table(DBTableList.REVIEW_TABLE).insert(reviewList)
+}
